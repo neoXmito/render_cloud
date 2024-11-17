6add98d490aa37e0efe0c005b6e0fa2e545fd5f9
malama chali <chalimalama@gmail.com>
2024-11-17 13:13:19 +0100

    update

--- multipliers_collector_render.py
+++ multipliers_collector_render.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException, NoSuchWindowException
 from selenium.webdriver.support import expected_conditions as EC
+import chromedriver_autoinstaller
 
 # Configure logging (stdout will be captured by Render logs)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -19,11 +20,15 @@ telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 phone_number = os.getenv("PHONE_NUMBER")
 password = os.getenv("PASSWORD")
 
+# Install ChromeDriver if not installed or if the installed version is wrong
+chromedriver_autoinstaller.install()
+
 # Initialize WebDriver (headless mode for Render)
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')  # Run in headless mode
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')  # To handle GPU issues in headless mode
 driver = None
 hourly_multipliers = []  # Define hourly_multipliers globally
 
@@ -101,7 +106,6 @@ def navigate_to_aviator(driver):
 
     except Exception as e:
         handle_error(f"Error navigating to Aviator: {e}")
-        # Handle error, possibly return to home page or retry
 
 # Function to return to home page of BetPawa
 def return_to_home_page():
